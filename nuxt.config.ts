3e04cff02edc75117767e65bdfff6624cffa5bf0
shelho <boltievibragim9@gmail.com>
2024-11-04 21:58:16 +0300

    отправка

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,9 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
+  css: ['@/assets/style/global.css'],
   modules: ['@pinia/nuxt', 'nuxt-windicss',],
+  pinia: {
+    storesDirs: ['./stores/**'],
+  },
 })
